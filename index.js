c6d2ce9541570c2ed65a7db5ef22b75a4e7da531
Precilitha-T <95769163+Precilitha-T@users.noreply.github.com>
2025-04-09 20:21:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello from Jenkins Pipeline!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
